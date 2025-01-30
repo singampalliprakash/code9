43df6b3e68509289e045d80982ff0d66e996cd5c
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-01-30 23:02:09 +0530

    Add files via upload

---
+++ bill calculations.java
@@ -0,0 +1,33 @@
+class Main {
+    public static void main(String[] args) {
+        
+        
+    // double units=251,bill=0;
+    // if(units>=0){
+    //     if(units>=0&&units<=100){
+    //         bill=units*1.3;
+    //     }
+    //     else if(units>=101&&units<=200){
+    //         bill=(100*1.3)+(units-100)*2.5;
+    //     }
+    //     else if(units>=200&& units<=300){
+    //         bill=(100*1.3)+(100*2.5)+(units-200)*3.5;
+    //     }
+    //     System.out.println(+bill);
+    // }
+    
+    // else{
+    //     System.out.println("invalid units");
+    // }
+        
+        
+        
+        // int x=9545;
+        // int metre =x/100;
+        // int rm=x%100;
+        // int feet=rm/30;
+        // int rf=rm%30;
+        // double inches=rf/2.54;
+        // System.out.println("metres"+metre+"'"+"feet"+feet+","+"inches"+ inches);
+    }
+}
